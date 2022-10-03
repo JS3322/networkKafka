569bbc079755710dd0aee2f9eedfb06fc1c317b1
js3322 <js@cleancode.kr>
2022-10-03 14:41:16 +0900

    init

---
+++ src/main/java/com/example/networkkafka/config/kafkaTopConfig.java
@@ -0,0 +1,15 @@
+package com.example.networkkafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class kafkaTopConfig {
+
+    @Bean
+    public NewTopic amigoscodeTopic() {
+        return TopicBuilder.name("amigoscode").build();
+    }
+}
